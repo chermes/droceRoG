c3a50eb1f13668a9cb2e92fae2598b1cdb06aa60
Christoph Hermes <hermes@hausmilbe.net>
2012-01-05 21:38:57 +0100

    removed old methods

--- src/drocerog.c
+++ src/drocerog.c
@@ -12,7 +12,6 @@ ifont *times12;
 /* prototypes */
 int main_handler(int type, int par1, int par2);
 void msg(char *s);
-// void mainscreen_repaint();
 
 void msg(char *s) 
 {
@@ -53,7 +52,6 @@ int main_handler(int type, int par1, int par2)
 
     if (type == EVT_SHOW) {
         // occurs when this event handler becomes active
-        // mainscreen_repaint();
         gogame_draw_fullrepaint();
 
     }
@@ -84,8 +82,12 @@ int main_handler(int type, int par1, int par2)
                 msg("KEY_DOWN");
                 break;
 
-            case KEY_MUSIC:
-                msg("KEY_MUSIC");
+            case KEY_NEXT:
+                msg("KEY_NEXT");
+                break;
+
+            case KEY_PREV:
+                msg("KEY_PREV");
                 break;
 
             case KEY_MENU:
@@ -100,88 +102,11 @@ int main_handler(int type, int par1, int par2)
     }
 
     if (type == EVT_EXIT) {
-        // occurs only in main handler when exiting or when SIGINT received.
-        // save configuration here, if needed
-        // board_cleanup();
+        gogame_cleanup();
     }
 
     return 0;
 }
-
-// void mainscreen_repaint() 
-// {
-
-  // char buf[64];
-  // ibitmap *b;
-  // int i;
-
-  // ClearScreen();
-
-  // SetFont(times12, BLACK);
-  // DrawString(5, 2, "droceRoG - Go Game Record Viewer");
-
-  // board_draw_update(0);
-
-  // DrawBitmap(0, 20, &background);
-  // DrawBitmap(120, 30, &books);
-
-  // DrawLine(5, 500, 595, 500, BLACK);
-  // DrawLine(5, 502, 595, 502, DGRAY);
-  // DrawLine(5, 504, 595, 504, LGRAY);
-  // DrawLine(19, 516, 20, 517, BLACK);
-  // DrawLine(22, 516, 23, 516, BLACK);
-
-  // for (i=5; i<595; i+=3) 
-      // DrawPixel(i, 507, BLACK);
-
-  // DrawRect(5, 510, 590, 10, BLACK);
-
-  // for (i=0; i<256; i++) FillArea(35+i*2, 524, 2, 12, i | (i << 8) | (i << 16));
-
-  // b = BitmapFromScreen(0, 520, 600, 20);
-  // DrawBitmap(0, 550, b);
-  // free(b);
-  // InvertArea(0, 550, 600, 20);
-  // DimArea(0, 575, 600, 10, BLACK);
-
-  // if (! orient) {
-    // Stretch(pic_example, IMAGE_GRAY8, 10, 10, 10, 10, 600,  6,  6, 0);
-    // Stretch(pic_example, IMAGE_GRAY8, 10, 10, 10, 20, 600, 10,  6, 0);
-    // Stretch(pic_example, IMAGE_GRAY8, 10, 10, 10, 35, 600, 30,  6, 0);
-    // Stretch(pic_example, IMAGE_GRAY8, 10, 10, 10, 10, 610,  6, 10, 0);
-    // Stretch(pic_example, IMAGE_GRAY8, 10, 10, 10, 20, 610, 10, 10, 0);
-    // Stretch(pic_example, IMAGE_GRAY8, 10, 10, 10, 35, 610, 30, 10, 0);
-    // Stretch(pic_example, IMAGE_GRAY8, 10, 10, 10, 10, 625,  6, 30, 0);
-    // Stretch(pic_example, IMAGE_GRAY8, 10, 10, 10, 20, 625, 10, 30, 0);
-    // Stretch(pic_example, IMAGE_GRAY8, 10, 10, 10, 35, 625, 30, 30, 0);
-  // }
-
-  // SetFont(arial8n, BLACK);
-  // DrawString(350, 600, "Arial 8 with no antialiasing");
-  // SetFont(arial12, BLACK);
-  // DrawString(350, 615, "Arial 12 regular");
-  // SetFont(arialb12, BLACK);
-  // DrawString(350, 630, "Arial 12 bold");
-  // SetFont(cour16, BLACK);
-  // DrawString(350, 645, "Courier 16");
-  // SetFont(cour24, BLACK);
-  // DrawString(350, 660, "Courier 24");
-  // DrawSymbol(500, 660, ARROW_LEFT);
-  // DrawSymbol(520, 660, ARROW_RIGHT);
-  // DrawSymbol(540, 660, ARROW_UP);
-  // DrawSymbol(560, 660, ARROW_DOWN);
-  // SetFont(times20, BLACK);
-  // DrawString(350, 680, "Times 20");
-  // DrawSymbol(450, 680, ARROW_LEFT);
-  // DrawSymbol(470, 680, ARROW_RIGHT);
-  // DrawSymbol(490, 680, ARROW_UP);
-  // DrawSymbol(510, 680, ARROW_DOWN);
-
-  //DrawTextRect(25, 400, 510, 350, sometext, ALIGN_LEFT);
-
-  // FullUpdate();
-
-// }
 
 int main()
 {
