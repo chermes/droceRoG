381265595d5181e5bf2ebd5ecabdfad9d38ca2c9
Christoph Hermes <hermes@hausmilbe.net>
2012-02-12 23:09:26 +0100

    creation of drocerog.app and drocerog.exe

--- makearm.sh
+++ makearm.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 cmake -DCMAKE_BUILD_TYPE=Release -DTARGET_TYPE=ARM
 make
-
+if [ -e drocerog ]; then
+    mv drocerog drocerog.app
+fi
--- makepc.sh
+++ makepc.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 cmake -DCMAKE_BUILD_TYPE=Debug -DTARGET_TYPE=Linux
 make
-
+if [ -e drocerog ]; then
+    mv drocerog drocerog.exe
+fi
